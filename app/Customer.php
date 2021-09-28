c1d3198e02c1642b2100c535b7c94fdaf47a4380
Oleksii Derzhuk <oleksii.derzhuk@gmail.com>
2021-09-28 14:34:39 +0300

    Tables migrations & models relations finished

--- app/Customer.php
+++ app/Customer.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Customer extends Model
 {
-    //
-}
+    public function flights(){
+        return $this->belongsToMany('App\Flight');
+    }}
+ 
--- app/Flight.php
+++ app/Flight.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Flight extends Model
 {
-    //
+    public function arrivalAirport(){
+        return $this->belongsTo('App\Airport', 'arrivalAirport_id');
+    }
+
+    public function departureAirport(){
+        return $this->belongsTo('App\Airport', 'departureAirport_id');
+    }
+
+    public function passengers(){
+        return $this->belongsToMany('App\Customer', 'flight_customer');
+    }
 }
