87c6b80c414bf09e3165688560a99c700ca18d92
Oleksii Derzhuk <oleksii.derzhuk@gmail.com>
2021-09-28 14:02:36 +0300

    Airport model configured

--- app/Airport.php
+++ app/Airport.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Airport extends Model
 {
-    //
+    public function arrivingFlights(){
+        return $this->hasany('App\Flight','arrivalAirport_id');
+    }
+
+    public function departingFlights(){
+        return $this->hasany('App\Flight','departureAirport_id');
+    }
 }
